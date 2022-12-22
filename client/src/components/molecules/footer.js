57bda9c7f757598909d14afebebced6d8253650d
body0922@naver.com <body0922@naver.com>
2022-12-22 22:31:59 +0900

    design updated

--- client/src/components/molecules/footer.js
+++ client/src/components/molecules/footer.js
@@ -3,8 +3,10 @@ import React from "react";
 const Footer = () => {
   return (
     <div id="footer">
-      Copyright 2022. GALMURY All rights reserved. WARD.GG isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing
-      League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
+      <div id="copyright">
+        Copyright 2022. GALMURY All rights reserved. WARD.GG isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing
+        League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
+      </div>
     </div>
   );
 };
